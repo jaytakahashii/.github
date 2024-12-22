b2eedaad6176305f367b2b0642d57cb88fd326ed
Jay Takahashi <qsakura14555@gmail.com>
2024-12-23 02:58:18 +0900

    feat(SRC): ✨ TypeやScorpをカスタム

--- changelog.config.js
+++ changelog.config.js
@@ -1,70 +1,32 @@
 module.exports = {
   disableEmoji: false,
   format: '{type}{scope}: {emoji}{subject}',
-  list: ['test', 'feat', 'fix', 'chore', 'docs', 'refactor', 'style', 'ci', 'perf'],
+  list: ['feat', 'fix', 'refactor'], // Only required types
   maxMessageLength: 64,
   minMessageLength: 3,
-  questions: ['type', 'scope', 'subject', 'body', 'breaking', 'issues', 'lerna'],
-  scopes: [],
+  questions: ['type', 'scope', 'subject', 'body'], // Mandatory questions
+  scopes: ['SRC', 'DOC', 'CI', 'ENV'], // Changed to an array of strings
   types: {
-    chore: {
-      description: 'Build process or auxiliary tool changes',
-      emoji: '🤖',
-      value: 'chore'
-    },
-    ci: {
-      description: 'CI related changes',
-      emoji: '🎡',
-      value: 'ci'
-    },
-    docs: {
-      description: 'Documentation only changes',
-      emoji: '✏️',
-      value: 'docs'
-    },
     feat: {
       description: 'A new feature',
-      emoji: '🎸',
-      value: 'feat'
+      emoji: '✨',
+      value: 'feat',
     },
     fix: {
       description: 'A bug fix',
       emoji: '🐛',
-      value: 'fix'
-    },
-    perf: {
-      description: 'A code change that improves performance',
-      emoji: '⚡️',
-      value: 'perf'
+      value: 'fix',
     },
     refactor: {
-      description: 'A code change that neither fixes a bug or adds a feature',
+      description: 'A code change that neither fixes a bug nor adds a feature',
       emoji: '💡',
-      value: 'refactor'
+      value: 'refactor',
     },
-    release: {
-      description: 'Create a release commit',
-      emoji: '🏹',
-      value: 'release'
-    },
-    style: {
-      description: 'Markup, white-space, formatting, missing semi-colons...',
-      emoji: 'a',
-      value: 'style'
-    },
-    test: {
-      description: 'Adding missing tests',
-      emoji: '💍',
-      value: 'test'
-    },
-    messages: {
-      type: 'Select the type of change that you\'re committing:',
-      customScope: 'Select the scope this component affects:',
-      subject: 'Write a short, imperative mood description of the change:\n',
-      body: 'Provide a longer description of the change:\n ',
-      breaking: 'List any breaking changes:\n',
-      footer: 'Issues this commit closes, e.g #123:',
-      confirmCommit: 'The packages that this commit has affected\n',
-    },
-  }
+  },
+  messages: {
+    type: 'Select the type of change you are committing:',
+    scope: 'Select the scope of your changes:',
+    subject: 'Write a brief description of the change:\n',
+    body: 'Provide a detailed description of the change (optional):\n',
+  },
 };
